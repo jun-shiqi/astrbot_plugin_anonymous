b90ffab5b47096167d73218a98a06144a3fe5449
君落尘 <9475665+you-fall-into-the-dust@user.noreply.gitee.com>
2025-02-18 21:52:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ class MyPlugin(Star):
         super().__init__(context)
     
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("opl")
-    async def opl(self, event: AstrMessageEvent,massage :str,g_id:str):
+    @filter.command("匿名")
+    async def opl(self, event: AstrMessageEvent):
         '''这是一个 hello world 指令''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
         if event.get_platform_name() == "aiocqhttp":
             from astrbot.core.platform.sources.aiocqhttp.aiocqhttp_message_event import AiocqhttpMessageEvent
             assert isinstance(event, AiocqhttpMessageEvent)
             client = event.bot # 得到 client
+            massage_list=event.message_str.split(" ")
+            g_id=massage_list[1]
+            massage=massage_list[2]
             payloads = {
                 "group_id": g_id,
                 "message": [

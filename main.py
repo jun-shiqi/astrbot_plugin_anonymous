421ad70bc7b34aa2d05412e9480506c6c0029bfc
君落尘 <9475665+you-fall-into-the-dust@user.noreply.gitee.com>
2025-02-18 21:56:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class MyPlugin(Star):
         super().__init__(context)
     
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("匿名")
+    @filter.command("opl")
     async def opl(self, event: AstrMessageEvent):
         '''这是一个 hello world 指令''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
         if event.get_platform_name() == "aiocqhttp":
